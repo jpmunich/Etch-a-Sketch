ac2dda4ff49260613675977761a85a8964375e46
jpmunich <parkercoyle8@gmail.com>
2022-08-14 08:06:30 -0400

    Can draw and erase on the grid

--- script.js
+++ script.js
@@ -1,13 +1,19 @@
-const gridContainer = document.getElementById("grid-container");
-
+const gridContainer = document.getElementById("grid-element-container");
+const drawButton = document.getElementById("draw-button");
+const eraseButton = document.getElementById("erase-button");
+let isDrawClicked = false;
+let isEraseClicked = false;
 
 function makeGridPiece() {
     const gridElement = document.createElement("div");
     gridContainer.appendChild(gridElement);
-    gridElement.classList.add("grid");
+
+    // Adds to the container div
+    gridElement.classList.add("grid-element");
+    gridElementsList = document.querySelectorAll(".grid-element");
 }
 
-
+// Parameter "size" determines how many grid pieces will be made
 function makeGrid(size) {
     for (let i = 0; i < size; i++) {
         makeGridPiece();
@@ -15,3 +21,31 @@ function makeGrid(size) {
 }
 
 makeGrid(25);
+
+drawButton.addEventListener("click", function() {
+    isDrawClicked = true;
+    isEraseClicked = false;
+})
+
+eraseButton.addEventListener("click", function() {
+    isEraseClicked = true;
+    isDrawClicked = false;
+})
+
+for (let i = 0; i < gridElementsList.length; i++) {
+    gridElementsList[i].addEventListener("click", function() {
+        if (isDrawClicked) {
+        gridElementsList[i].classList.add("clicked-grid-element");
+        }
+    })
+
+    gridElementsList[i].addEventListener("click", function() {
+        if (isEraseClicked) {
+        gridElementsList[i].classList.remove("clicked-grid-element");
+        }
+    })
+}
+
+
+
+
